4b90b063593e9aa7a67701fecf3f96f4fc95aa78
softwareNinja-888 <mlandvodlamini03@gmail.com>
2025-01-17 21:16:43 +0200

    Initial commit

---
+++ ERP/tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        geist: ['geist','sans-serif'],
+        lora: ['lora','sans-serif'],
+        nunito: ['nunito','sans-serif'],
+        poppins: ['poppins','sans-serif'],
+        source: ['source','sans-serif'],
+        fira: ['fira','sans-serif'],
+        inter: ['inter','sans-serif'],
+        roboto: ['roboto','sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+}
